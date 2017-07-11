b708ec79c9d4c93a5e379ea7997d6e0012905d1d
malashkovv <malashkovv@gmail.com>
2017-07-11 18:13:22 +0300

    PostgreSQL configuration. First queries.

---
+++ postgresql/queries/user.sql
@@ -0,0 +1,9 @@
+DROP TABLE users;
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL
+);
+
+INSERT INTO users (first_name, last_name) VALUES('Greg', 'Lira');
+select * from users;
