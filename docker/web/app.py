25f8bdaeeeb06f243bab9da448b83687f67443d1
valentin <malashkovv@gmail.com>
2017-07-12 23:16:29 +0300

    Improvements

--- docker/web/app.py
+++ docker/web/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import socket
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-  return 'Hello world'
+  return "Hello, I'm {}".format(socket.gethostname())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
